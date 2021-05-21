c6f467e9611723db5af3f39ef00ef27190bd0fd5
kitajii <kazu.rmcf@gmail.com>
2021-05-21 16:38:36 +0900

    second commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '2e9feb3b2c5d35178cfae6c834494f11b3485e0e',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '2e9feb3b2c5d35178cfae6c834494f11b3485e0e',
+    ),
+  ),
+);
